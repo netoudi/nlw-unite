92f4376961ba19fe82cdf8eb92c34d0e7aef694e
netoudi <netoudi@rocketmail.com>
2024-04-03 21:54:00 -0300

    feat(server): add endpoint events

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,11 @@
+import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
+import { z } from 'zod';
+import { generateSlug } from '@/utils/generate-slug';
+
+const prisma = new PrismaClient({
+  log: ['query'],
+});
 
 const app = fastify();
 
@@ -6,10 +13,43 @@ app.get('/', async () => {
   return { message: 'Hello world!' };
 });
 
+app.post('/events', async (request, replay) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  });
+
+  const { title, details, maximumAttendees } = createEventSchema.parse(request.body);
+
+  const slug = generateSlug(title);
+
+  const eventWithSameSlug = await prisma.event.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (eventWithSameSlug !== null) {
+    throw new Error('Another event with same title already exists');
+  }
+
+  const event = await prisma.event.create({
+    data: {
+      title,
+      details,
+      maximumAttendees,
+      slug,
+    },
+  });
+
+  return replay.status(201).send({ eventId: event.id });
+});
+
 app
   .listen({
     host: '0.0.0.0',
-    port: parseInt(process.env.PORT ?? 3333),
+    port: Number(process.env.PORT ?? 3333),
   })
   .then(() => {
     console.log(`🚀 HTTP server listening on http://localhost:3333`);
