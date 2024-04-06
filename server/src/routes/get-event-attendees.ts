ac657fdf7d16f8c2a572b7bf5400af579168ccf1
netoudi <netoudi@rocketmail.com>
2024-04-06 14:54:54 -0300

    feat(server): add endpoint to list attendees from event

---
+++ server/src/routes/get-event-attendees.ts
@@ -0,0 +1,68 @@
+import { type FastifyInstance } from 'fastify';
+import { type ZodTypeProvider } from 'fastify-type-provider-zod';
+import { z } from 'zod';
+import { prisma } from '@/lib/prisma';
+
+export async function getEventAttendees(app: FastifyInstance): Promise<void> {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/events/:eventId/attendees',
+    {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        querystring: z.object({
+          query: z.string().nullish(),
+          pageIndex: z.string().nullish().default('0').transform(Number),
+        }),
+        response: {
+          200: z.object({
+            attendees: z.array(
+              z.object({
+                id: z.number().int(),
+                name: z.string(),
+                email: z.string().email(),
+                createdAt: z.date(),
+                checkedInAt: z.date().nullable(),
+              }),
+            ),
+          }),
+        },
+      },
+    },
+    async (request, replay) => {
+      const { eventId } = request.params;
+      const { pageIndex, query } = request.query;
+
+      const attendees = await prisma.attendee.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          createdAt: true,
+          checkIn: {
+            select: {
+              createdAt: true,
+            },
+          },
+        },
+        where: query !== null ? { eventId, name: { contains: query } } : { eventId },
+        take: 10,
+        skip: pageIndex * 10,
+        orderBy: { createdAt: 'desc' },
+      });
+
+      return replay.status(200).send({
+        attendees: attendees.map((attendee) => {
+          return {
+            id: attendee.id,
+            name: attendee.name,
+            email: attendee.email,
+            createdAt: attendee.createdAt,
+            checkedInAt: attendee.checkIn?.createdAt ?? null,
+          };
+        }),
+      });
+    },
+  );
+}
