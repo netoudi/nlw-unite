575e42abe2d4b1dee624211616a3507c84860be9
netoudi <netoudi@rocketmail.com>
2024-04-06 20:41:00 -0300

    feat(web): add component input-search

--- web/src/components/attendee-list.tsx
+++ web/src/components/attendee-list.tsx
@@ -1,3 +1,10 @@
+import { InputSearch } from '@/components/input-search';
+
 export function AttendeeList() {
-  return <div>AttendeeList</div>;
+  return (
+    <div className="flex gap-3 items-center">
+      <h1 className="text-2xl font-bold">Participantes</h1>
+      <InputSearch />
+    </div>
+  );
 }
---
+++ web/src/components/input-search.tsx
@@ -0,0 +1,18 @@
+import { ComponentProps } from 'react';
+import { Search } from 'lucide-react';
+
+interface InputSearchProps extends ComponentProps<'input'> {}
+
+export function InputSearch(props: InputSearchProps) {
+  return (
+    <div className="px-3 w-72 py-1.5 border border-white/10  rounded-lg text-sm flex items-center gap-3">
+      <Search className="size-4 text-emerald-300" />
+      <input
+        type="text"
+        className="bg-transparent flex-1 outline-none"
+        placeholder="Buscar participante..."
+        {...props}
+      />
+    </div>
+  );
+}
