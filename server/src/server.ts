1734220b206c23ffa885113b331b08a75ce3ffe6
netoudi <netoudi@rocketmail.com>
2024-04-06 11:31:58 -0300

    refactor(server): move endpoint events to file routes/create-event.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,6 @@
-import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
-import { type ZodTypeProvider, serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
-import { z } from 'zod';
-import { generateSlug } from '@/utils/generate-slug';
-
-const prisma = new PrismaClient({
-  log: ['query'],
-});
+import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
+import { createEvent } from '@/routes/create-event';
 
 const app = fastify();
 
@@ -18,49 +12,7 @@ app.get('/', async () => {
   return { message: 'Hello world!' };
 });
 
-app.withTypeProvider<ZodTypeProvider>().post(
-  '/events',
-  {
-    schema: {
-      body: z.object({
-        title: z.string().min(4),
-        details: z.string().nullable(),
-        maximumAttendees: z.number().int().positive().nullable(),
-      }),
-      response: {
-        201: z.object({
-          eventId: z.string().uuid(),
-        }),
-      },
-    },
-  },
-  async (request, replay) => {
-    const { title, details, maximumAttendees } = request.body;
-
-    const slug = generateSlug(title);
-
-    const eventWithSameSlug = await prisma.event.findUnique({
-      where: {
-        slug,
-      },
-    });
-
-    if (eventWithSameSlug !== null) {
-      throw new Error('Another event with same title already exists');
-    }
-
-    const event = await prisma.event.create({
-      data: {
-        title,
-        details,
-        maximumAttendees,
-        slug,
-      },
-    });
-
-    return replay.status(201).send({ eventId: event.id });
-  },
-);
+app.register(createEvent);
 
 app
   .listen({
