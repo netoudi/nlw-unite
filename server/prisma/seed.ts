0af0efb41b6446ecacbe5225bcc22bfd245442bd
netoudi <netoudi@rocketmail.com>
2024-04-09 23:13:13 -0300

    feat(server): add seed to attendees

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,4 +1,5 @@
 import { prisma } from '../src/lib/prisma';
+import { faker } from '@faker-js/faker';
 
 async function seed(): Promise<void> {
   await prisma.event.create({
@@ -10,6 +11,16 @@ async function seed(): Promise<void> {
       maximumAttendees: 100,
     },
   });
+  const attendees = Array.from({ length: 100 }).map((_, index) => {
+    return {
+      id: index + 1,
+      eventId: '0880348a-3a6c-4719-a320-cf3a1f4d3238',
+      name: faker.person.fullName(),
+      email: faker.internet.email().toLowerCase(),
+      createdAt: faker.date.recent({ days: 30 }),
+    };
+  });
+  await prisma.attendee.createMany({ data: attendees });
 }
 
 seed().then(() => {
